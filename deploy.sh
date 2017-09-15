95574c583e94809aae57f6cc0c0027b3fe405d1b
Lucas van Lierop <me@lucasvanlierop.nl>
2017-09-15 05:09:56 +0000

    Adds initial production (deploy) nfig

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+SWARM_HOST=lucasvanlierop-website
+DOCKER_COMPOSE_FILE_PROD=environment/prod/docker-compose.yml
+
+deploy_stack() {
+    local stack_name=$1
+    local stack_file=$2
+
+    eval $(docker-machine env ${SWARM_HOST})
+
+    docker \
+		stack deploy \
+		-c ${stack_file} \
+		${stack_name}
+
+    eval $(docker-machine env --unset)
+}
+
+deploy_stack allihoppa ${DOCKER_COMPOSE_FILE_PROD}
