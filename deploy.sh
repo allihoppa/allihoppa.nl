7751a7471f5baf0880eb6e2c638206d5d43a86b4
Lucas van Lierop <me@lucasvanlierop.nl>
2017-09-25 17:48:56 +0000

    Allows deploying a specific commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
-
+COMMIT_HASH=$1
 SWARM_HOST=lucasvanlierop-website
 DOCKER_COMPOSE_FILE_PROD=environment/prod/docker-compose.yml
 export ALLIHOPPA_UID=1001
 export ALLIHOPPA_GID=1001
-export DEPLOY_TAG=$(./deploy/bin/generate-deploy-tag)
+export DEPLOY_TAG=$(./deploy/bin/generate-deploy-tag ${COMMIT_HASH})
+
 
 deploy_stack() {
     local stack_name=$1
