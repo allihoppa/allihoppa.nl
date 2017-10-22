089b63116c53dac8c1533a358091feaac157e445
Lucas van Lierop <me@lucasvanlierop.nl>
2017-10-22 09:24:02 +0000

    space formatting

--- public/wp-config.php
+++ public/wp-config.php
@@ -13,13 +13,11 @@ define('AUTH_SALT', getenv('AUTH_SALT'));
 define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
 define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
 define('NONCE_SALT', getenv('NONCE_SALT'));
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 define('WPLANG', 'nl_NL');
-define('WP_AUTO_UPDATE_CORE', false );
+define('WP_AUTO_UPDATE_CORE', false);
 define('WP_ENV', getenv('WP_ENV'));
 define('WP_DEBUG', getenv('WP_DEBUG') === 'true');
-define('WP_CONTENT_DIR',    __DIR__ . '/wp-content');
-define('WP_CONTENT_URL',    WP_HOME . '/wp-content');
 function generateWpHome()
 {
     $httpHostParts = parse_url($_SERVER['HTTP_HOST']);
@@ -32,6 +30,9 @@ function generateWpHome()
 define('WP_HOME', generateWpHome());
 define('WP_SITEURL', WP_HOME . '/wordpress');
 
+define('WP_CONTENT_DIR', __DIR__ . '/wp-content');
+define('WP_CONTENT_URL', WP_HOME . '/wp-content');
+
 define('WP_SMTP_HOST', getenv('WP_SMTP_HOST'));
 define('WP_SMTP_PORT', (int) getenv('WP_SMTP_PORT'));
 define('WP_SMTP_ENCRYPTION', getenv('WP_SMTP_ENCRYPTION'));
@@ -40,8 +41,8 @@ define('WP_SMTP_PASSWORD', getenv('WP_SMTP_PASSWORD'));
 define('WP_SMTP_FROM', getenv('WP_SMTP_FROM'));
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+if (!defined('ABSPATH'))
+    define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
