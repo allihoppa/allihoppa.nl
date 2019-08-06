c72996f1bd58f60d43c4ad48b7001354e333bc16
Lucas van Lierop <me@lucasvanlierop.nl>
2019-08-06 17:04:20 +0200

    Adds support for FTP hosting where no environment vars are set

---
+++ configFromEnv.php
@@ -0,0 +1,23 @@
+<?php
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
+define('WP_ENV', getenv('WP_ENV'));
+define('WP_DEBUG', getenv('WP_DEBUG') === 'true');
+define('WP_SMTP_HOST', getenv('WP_SMTP_HOST'));
+define('WP_SMTP_PORT', (int) getenv('WP_SMTP_PORT'));
+define('WP_SMTP_ENCRYPTION', getenv('WP_SMTP_ENCRYPTION'));
+define('WP_SMTP_USER', getenv('WP_SMTP_USER'));
+define('WP_SMTP_PASSWORD', getenv('WP_SMTP_PASSWORD'));
+define('WP_SMTP_FROM', getenv('WP_SMTP_FROM'));
--- public/wp-config.php
+++ public/wp-config.php
@@ -1,24 +1,16 @@
 <?php
+$pathToConfig = '../private/configProduction.php';
+if (file_exists($pathToConfig)) {
+    require_once $pathToConfig;
+} else {
+    require_once '../configFromEnv.php';
+}
+
 const LOCALHOST = 'localhost';
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
-define('AUTH_KEY', getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY', getenv('NONCE_KEY'));
-define('AUTH_SALT', getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT', getenv('NONCE_SALT'));
+
 $table_prefix = 'wp_';
 define('WPLANG', 'nl_NL');
 define('WP_AUTO_UPDATE_CORE', false);
-define('WP_ENV', getenv('WP_ENV'));
-define('WP_DEBUG', getenv('WP_DEBUG') === 'true');
 convinceWordPressToUseHttpsProtocolInUrlsWhenBehindAnSslOffloadingProxy();
 function generateWpHome()
 {
@@ -40,13 +32,6 @@ define('INTERNAL_WP_HOME', 'http://' . LOCALHOST . ':' . getenv('HTTP_PORT'));
 
 define('WP_CONTENT_DIR', __DIR__ . '/wp-content');
 define('WP_CONTENT_URL', WP_HOME . '/wp-content');
-
-define('WP_SMTP_HOST', getenv('WP_SMTP_HOST'));
-define('WP_SMTP_PORT', (int) getenv('WP_SMTP_PORT'));
-define('WP_SMTP_ENCRYPTION', getenv('WP_SMTP_ENCRYPTION'));
-define('WP_SMTP_USER', getenv('WP_SMTP_USER'));
-define('WP_SMTP_PASSWORD', getenv('WP_SMTP_PASSWORD'));
-define('WP_SMTP_FROM', getenv('WP_SMTP_FROM'));
 
 /** Absolute path to the WordPress directory. */
 if (!defined('ABSPATH'))
